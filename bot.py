7872ccbc60be8c9a4975bd3a887bd9f897d71900
David Crawford <DaveAldon@users.noreply.github.com>
2020-06-11 15:26:15 -0400

    Removed dotenv ref. Discord has its own key structure that is easier to reference.

--- bot.py
+++ bot.py
@@ -2,9 +2,7 @@ import os
 import random
 
 from discord.ext import commands
-from dotenv import load_dotenv
 
-load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
